68119b44e4a156b4d0a26379c1ad89e245a4cdcf
juanf1021 <82191280+juanf1021@users.noreply.github.com>
2024-03-02 21:06:51 -0500

    graphiz agregado listo para empezar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Graphviz } from 'graphviz-react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Buena Juliancho</h1>
+      <h2> Un grafo</h2>
+      <Graphviz
+        dot={`digraph {
+          a -> b[label="0.2",weight="0.2"];
+          a -> c[label="0.4",weight="0.4"];
+          c -> b[label="0.6",weight="0.6"];
+          c -> e[label="0.6",weight="0.6"];
+          e -> e[label="0.1",weight="0.1"];
+          e -> b[label="0.7",weight="0.7"];
+      }`}
+      ></Graphviz>
+
+      <Graphviz
+        dot={`graph {
+          a -- b[color=red,penwidth=3.0];
+          b -- c;
+          c -- d[color=red,penwidth=3.0];
+          d -- e;
+          e -- f;
+          a -- d;
+          b -- d[color=red,penwidth=3.0];
+          c -- f[color=red,penwidth=3.0];
+      }`}
+      ></Graphviz>
     </div>
   );
 }
